cdd279147ca4ae8b05c9295563f1f3b55a7ff7a0
hri <hridyapraisevm@gmail.com>
2024-09-18 11:52:16 +0530

    added index files

---
+++ voting-app-frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css'; // You can remove this if you don't have an index.css
+import App from './App'; // App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
